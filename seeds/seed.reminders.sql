a49366a5330911fbebba5fed54094e9070c07f05
kd Bromley <kd.bromley1@gmail.com>
2021-03-06 18:52:48 -0800

    add migrations

---
+++ seeds/seed.reminders.sql
@@ -0,0 +1,9 @@
+INSERT INTO reminders
+    (title, due_date, reminder_notes, completed, user)
+VALUES
+('First reminder', '2021-03-06 18:00', 'This is my first reminder!', TRUE, 1),
+('Water plants', '2021-03-10 10:00', 'Spritz flowers with water', FALSE, 1),
+('Check on neighbor`s cat 19:30' , '2021-03-07', 'Food and water bowl, litter', null, 1),
+('Fullstack capstone', '2021-02-26 00:00', '', FALSE, 1),
+('Credit card bill', '2021-03-15 00:00 ', '', TRUE, 1),
+('Bring trash in from curb', '2021-03-16 14:00', '', null, 1);
---
+++ seeds/seed.users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users
+    (username, password, email, full_name)
+VALUES
+('dummyuser', 'dummypassword', 'email@host.com', 'Al Einstein');
