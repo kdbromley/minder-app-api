a5d051017fc4af8c3666867e1783c2d12460214d
kd Bromley <kd.bromley1@gmail.com>
2021-03-06 23:40:27 -0800

    GET /reminders/:reminderId functional

--- src/reminders/reminders-router.js
+++ src/reminders/reminders-router.js
@@ -18,5 +18,23 @@ remindersRouter
         .catch(next)
     })
 
+remindersRouter
+    .route('/:reminderId')
+    .get((req, res, next)=> {
+        RemindersService.getReminderById(
+            req.app.get('db'),
+            req.params.reminderId
+        )
+        .then(reminder => {
+            if(!reminder) {
+                return res.status(404).json({
+                    error: { message: `Reminder does not exist`}
+                })
+            }
+            res.json(reminder)
+        })
+        .catch(next)
+    })
+
 
 module.exports = remindersRouter;
